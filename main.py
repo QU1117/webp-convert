22ee109ceec407253860a5072f0e0f843222549e
QU1117 <volgirre@gmail.com>
2023-07-20 19:58:09 +0300

    Version 0.0.1, naive and dirty solution, converts only to PNG and saves at root

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+from PIL import Image
+
+parser = argparse.ArgumentParser()
+parser.add_argument('directory')
+arguments = parser.parse_args()
+
+directory = os.fsencode(arguments.directory)
+count = 0
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+
+    if filename.endswith('.webp'):
+        image = Image.open(f'{arguments.directory + "/" + filename}')
+        new_filename = filename.split()[0] + '.png'
+        image.save(f'{new_filename}', format='PNG')
